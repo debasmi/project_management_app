2d985e74f03182259475c937a165b2a73b7dd43d
DEBASMI_BASU <basudebasmi2006@gmail.com>
2024-11-19 19:32:33 +0530

    2nd commit

--- workspace/project_management/project_management/settings.py
+++ workspace/project_management/project_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+LOGIN_REDIRECT_URL= 'dashboard'
+LOGIN_URL='login'
